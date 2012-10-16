f4d02babe77bdde972b4d8af501935d1c891e3e9
Alexey Kinyov <rudy@05bit.com>
2012-10-16 18:54:43 +0400

    (date) token now available for pattern in safe_upload_to helper

--- rango/utils.py
+++ rango/utils.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime
 from unidecode import unidecode
 from rango import crypto
 
 def safe_filename(name):
 	return unidecode(name)
 
-def safe_upload_to(prefix='', pattern=None, random_size=20):
+def safe_upload_to(prefix='', pattern=None, random_size=20, time_format=None):
 	"""
 	This helper provides collision-safe and some kind secure
 	file path maker, which can be used for ``upload_to``
@@ -21,6 +22,7 @@ def safe_upload_to(prefix='', pattern=None, random_size=20):
 			file = models.FileField(upload_to=safe_upload_to('files'))
 	"""
 	pattern = pattern or '%(prefix)s/%(pk)s/%(random)s_%(name)s'
+	time_format = time_format or '%Y-%m-%d'
 	bits = {}
 	if '(prefix)' in pattern:
 		bits['prefix'] = prefix or '_'
@@ -30,5 +32,7 @@ def safe_upload_to(prefix='', pattern=None, random_size=20):
 			bits['random'] = crypto.random_token(random_size)
 		if '(name)' in pattern:
 			bits['name'] = safe_filename(name)
+		if '(time)' in pattern:
+			bits['time'] = datetime.now().strftime(time_format)
 		return pattern % bits
 	return upload_to
