bef127213f0a97f8bd0f46ca298ca89ea5e24ec9
Alexey Kinyov <rudy@05bit.com>
2012-10-16 12:13:23 +0400

    added mark_safe unil method to rango.template

--- rango/template.py
+++ rango/template.py
@@ -5,6 +5,7 @@ from django.template.loader import select_template as select_template_
 from django.template import Library as Library_
 # from django.template import Context as Context_
 # from django.template import RequestContext as RequestContext_
+from django.utils.safestring import mark_safe as mark_safe_
 
 # Context = Context_
 # RequestContext = RequestContext_
@@ -12,6 +13,8 @@ Library = Library_
 get_template = get_template_
 select_template = select_template_
 TemplateSyntaxError = TemplateSyntaxError_
+mark_safe = mark_safe_
+
 
 def render(template_name, context_data, request=None):
 	template = get_template(template_name)
