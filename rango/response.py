60286ee12d670fab32d8a779c2e4a0edd2438189
Alexey Kinyov <rudy@05bit.com>
2014-05-09 15:33:50 +0400

    fix: removed  in JSON dump in JsonResponse as it caused crash when mixing unicode and pure ascii data

--- rango/response.py
+++ rango/response.py
@@ -2,8 +2,10 @@ from django.http import *
 from django.utils import simplejson
 
 class JsonResponse(HttpResponse):
-	"""
-	HttpResponse descendant, which return response with ``application/json`` mimetype.
-	"""
-	def __init__(self, data):
-		super(JsonResponse, self).__init__(content=simplejson.dumps(data, ensure_ascii=False), mimetype='application/json')
+    """
+    HttpResponse descendant, which return response with ``application/json`` mimetype.
+    """
+    def __init__(self, data):
+        content = simplejson.dumps(data, encoding='utf-8')
+        super(JsonResponse, self).__init__(content=content,
+                                           mimetype='application/json; charset=utf-8')
