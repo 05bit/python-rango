1e51673428defd115c76da592a240b1ac1ef1856
Alexey Kinyov <rudy@05bit.com>
2014-05-09 18:51:13 +0400

    render_to_string shortcut

--- rango/template.py
+++ rango/template.py
@@ -2,6 +2,7 @@ from django.template import *
 from django.template import TemplateSyntaxError as TemplateSyntaxError_
 from django.template.loader import get_template as get_template_
 from django.template.loader import select_template as select_template_
+from django.template.loader import render_to_string as render_to_string_
 from django.template import Library as Library_
 # from django.template import Context as Context_
 # from django.template import RequestContext as RequestContext_
@@ -14,6 +15,7 @@ get_template = get_template_
 select_template = select_template_
 TemplateSyntaxError = TemplateSyntaxError_
 mark_safe = mark_safe_
+render_to_string = render_to_string_
 
 
 def render(template_name, context_data, request=None):
